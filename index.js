ed7287de55ac00b6e50764c5a97d9db0b161bd2c
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-08 16:21:54 +0200

    deliver base products

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+
+const corsConfig = {
+	origin: process.env.ORIGIN,
+	optionsSuccessStatus: 200,
+	methods: "GET, POST",
+};
+
+app.use(cors(corsConfig));
 
 const products = [
 	{
